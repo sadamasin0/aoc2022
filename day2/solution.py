e73b08a5c7cc352eeb306008fbe3dcf5a1907c45
Adam Sasin <sadamasin@gmail.com>
2022-12-02 10:43:02 +0100

    day 2: part two

--- day2/solution.py
+++ day2/solution.py
@@ -1,24 +1,18 @@
-shapes_points = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3
-}
-
 result_points = {
     'X': {
         'A': 3,
-        'B': 0, 
-        'C': 6
+        'B': 1, 
+        'C': 2
     },
     'Y': {
-        'A': 6,
-        'B': 3,
-        'C': 0
+        'A': 4,
+        'B': 5,
+        'C': 6
     },
     'Z': {
-        'A': 0,
-        'B': 6,
-        'C': 3
+        'A': 8,
+        'B': 9,
+        'C': 7
     }
 }
 
@@ -28,6 +22,6 @@ input = open('input.txt').readlines()
 
 for round in input:
     choices = round.strip().split(' ')
-    total_points += shapes_points[choices[1]] + result_points[choices[1]][choices[0]]
+    total_points += result_points[choices[1]][choices[0]]
 
 print(total_points)
