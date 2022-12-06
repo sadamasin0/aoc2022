4eb0bcfee0674581dfb7b00201ccf15f5eb6d928
Adam Sasin <sadamasin@gmail.com>
2022-12-06 12:46:03 +0100

    day 6: part one

---
+++ day6/solution.py
@@ -0,0 +1,16 @@
+def check_if_all_unique(iter):
+    results = []
+    for x in iter:
+        copy_list = list(iter).copy()
+        copy_list.remove(x)
+        results.append(x not in copy_list)
+    return all(results)
+
+def input_lookup_for_marker():
+    input = open('input.txt').readline()
+    for counter in range(len(input)):
+        if check_if_all_unique(input[counter:counter+4]):
+            return counter
+
+if __name__ == '__main__':
+    print(input_lookup_for_marker()+4)
